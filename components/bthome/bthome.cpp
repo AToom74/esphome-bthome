b51560a881ba0aec5cc59adfa4dce2affae59e5c
AToom74 <169132079+AToom74@users.noreply.github.com>
2026-09-17 09:18:03 +0200

    Remove unused include for esp_nimble_hci.h
    Removed unnecessary include for esp_nimble_hci.h.

--- components/bthome/bthome.cpp
+++ components/bthome/bthome.cpp
@@ -11,7 +11,6 @@
 // Platform-specific includes
 #ifdef USE_ESP32
   #ifdef USE_BTHOME_NIMBLE
-    #include "esp_nimble_hci.h"
     #include "nimble/nimble_port.h"
     #include "nimble/nimble_port_freertos.h"
     #include "host/ble_hs.h"
