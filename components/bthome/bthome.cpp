44d10d579d2f3a57689bc81155b8d8fc5b4caae5
AToom74 <169132079+AToom74@users.noreply.github.com>
2026-09-17 11:12:44 +0200

    Add nimble_port.h include for ESP32 support

--- components/bthome/bthome.cpp
+++ components/bthome/bthome.cpp
@@ -11,6 +11,7 @@
 // Platform-specific includes
 #ifdef USE_ESP32
   #ifdef USE_BTHOME_NIMBLE
+    #include "nimble/nimble_port.h"
     #include "nimble/nimble_port_freertos.h"
     #include "host/ble_hs.h"
     #include "host/util/util.h"
